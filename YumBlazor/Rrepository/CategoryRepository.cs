96053519de1e46dc736805d35ba576066c21e88d
jay <jaychovatiya529@gmail.com>
2024-11-19 12:32:06 +0530

    Add Category Model Add Category Repository

---
+++ YumBlazor/Rrepository/CategoryRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using YumBlazor.Data;
+using YumBlazor.Rrepository.IRepository;
+
+namespace YumBlazor.Rrepository
+{
+	public class CategoryRepository : ICategoryRepository
+	{
+		private readonly ApplicationDbContext _db;
+
+		public CategoryRepository(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public Category Create(Category obj)
+		{
+			_db.Category.Add(obj);
+			_db.SaveChanges();
+			return obj;
+		}
+
+		public bool Delete(int id)
+		{
+			var obj = _db.Category.FirstOrDefault(u => u.Id == id);
+			if (obj != null)
+			{
+				_db.Category.Remove(obj);
+				return _db.SaveChanges() > 0;
+			}
+			return false;
+		}
+
+		public Category Get(int id)
+		{
+			var obj = _db.Category.FirstOrDefault(u => u.Id == id);
+			if (obj == null)
+			{
+				return new Category();
+			}
+			return obj;
+		}
+
+		public IEnumerable<Category> GetAII()
+		{
+			return _db.Category.ToList();
+		}
+
+		public Category Update(Category obj)
+		{
+			var objFromDb = _db.Category.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+				objFromDb.Name = obj.Name;
+				_db.Category.Update(objFromDb);
+				_db.SaveChanges();
+				return objFromDb;
+            }
+			return obj;
+        }
+	}
+}
